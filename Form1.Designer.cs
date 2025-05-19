9a87fc11e8d58d641555b1d195192874a0e61194
yu457 <113153008@gas.fgu.edu.tw>
2025-05-19 19:58:12 +0800

    鬧鐘功能
    1.匯入鬧鐘聲音(打開專案檔，找尋一個「bin」->「Debug」資料夾，在裡面新增一個資料夾，請將檔案名稱改為「Resources」)
    2.安裝「NuGet套件管理員」
    3.新增聲音檔播放之程式
    4.按鍵事件綁定及新增程式

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -143,6 +143,7 @@
             this.btnSetAlert.TabIndex = 3;
             this.btnSetAlert.Text = "設定鬧鐘";
             this.btnSetAlert.UseVisualStyleBackColor = true;
+            this.btnSetAlert.Click += new System.EventHandler(this.btnSetAlert_Click);
             // 
             // btnCancelAlert
             // 
@@ -152,6 +153,7 @@
             this.btnCancelAlert.TabIndex = 4;
             this.btnCancelAlert.Text = "關閉鬧鐘";
             this.btnCancelAlert.UseVisualStyleBackColor = true;
+            this.btnCancelAlert.Click += new System.EventHandler(this.btnCancelAlert_Click);
             // 
             // cmbHour
             // 
@@ -182,6 +184,10 @@
             // timerClcok
             // 
             this.timerClcok.Tick += new System.EventHandler(this.timerClcok_Tick);
+            // 
+            // timerAlert
+            // 
+            this.timerAlert.Tick += new System.EventHandler(this.timerAlert_Tick);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;           // 音效檔播放器函式庫
+using System.IO;             // 檔案讀取的IO函式庫
+
 
 namespace SimpleClock
 {
@@ -43,6 +46,72 @@ namespace SimpleClock
             txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
             txtDate.Text = DateTime.Now.ToString("yyyy-MM-dd");  // 顯示日期
             txtWeekDay.Text = DateTime.Now.ToString("dddd");     // 顯示星期幾
+        }
+
+        private string strSelectTime;                       // 鬧鐘時間設定
+        private WaveOutEvent waveOut;                       // 音效檔播放器
+        private AudioFileReader audioFileReader;            // 音效檔讀取器
+
+        private void timerAlert_Tick(object sender, EventArgs e)
+        {
+            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            {
+                try
+                {
+                    stopWaveOut();
+
+                    // 指定聲音檔的相對路徑，可以使用MP3
+                    string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+
+                    // 使用 AudioFileReader 來讀取聲音檔
+                    audioFileReader = new AudioFileReader(audioFilePath);
+
+                    // 初始化 WaveOutEvent
+                    waveOut = new WaveOutEvent();
+                    waveOut.Init(audioFileReader);
+
+                    // 播放聲音檔
+                    waveOut.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
+                }
+                finally
+                {
+                    timerAlert.Stop(); // 停止鬧鐘計時器
+                }
+            }
+        }
+
+        // 停止之前的播放
+        private void stopWaveOut()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+
+
+
+        private void btnSetAlert_Click(object sender, EventArgs e)
+        {
+            timerAlert.Start(); // 啟動鬧鐘計時器
+            btnSetAlert.Enabled = false;
+            btnCancelAlert.Enabled = true;
+            strSelectTime = cmbHour.SelectedItem.ToString() + ":" + cmbMin.SelectedItem.ToString(); // 擷取小時和分鐘的下拉選單文字，用來設定鬧鐘時間
+        }
+
+        private void btnCancelAlert_Click(object sender, EventArgs e)
+        {
+            stopWaveOut();     // 停止之前的播放
+            timerAlert.Stop(); // 停止鬧鐘計時器
+            btnSetAlert.Enabled = true;
+            btnCancelAlert.Enabled = false;
         }
     }
 }
